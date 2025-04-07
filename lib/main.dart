f53abfc6383c0bb5705b632e47c6f9f93c26b953
“Staxupgit config --global user.email “nuhshekhey@gmail.com <nuhshekhey@gmail.com>
2025-04-07 14:33:55 -0400

    added registersignin widget

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Firebase Auth Demo')),
@@ -39,6 +38,89 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
           children: <Widget>[RegisterSignInWidget(auth: _auth)],
         ),
       ),
+    );
+  }
+}
+
+class RegisterSignInWidget extends StatefulWidget {
+  final FirebaseAuth auth;
+
+  const RegisterSignInWidget({super.key, required this.auth});
+
+  @override
+  State<RegisterSignInWidget> createState() => _RegisterSignInWidgetState();
+}
+
+class _RegisterSignInWidgetState extends State<RegisterSignInWidget> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool isRegistering = true;
+  bool isSuccess = false;
+  String? userEmail;
+
+  void _submit() async {
+    try {
+      if (isRegistering) {
+        await widget.auth.createUserWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+      } else {
+        await widget.auth.signInWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+      }
+
+      setState(() {
+        isSuccess = true;
+        userEmail = _emailController.text;
+      });
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ProfileScreen(auth: widget.auth),
+        ),
+      );
+    } catch (e) {
+      setState(() {
+        isSuccess = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: _emailController,
+          decoration: InputDecoration(labelText: 'Email'),
+        ),
+        TextField(
+          controller: _passwordController,
+          obscureText: true,
+          decoration: InputDecoration(labelText: 'Password'),
+        ),
+        ElevatedButton(
+          onPressed: _submit,
+          child: Text(isRegistering ? 'Register' : 'Sign In'),
+        ),
+        Text(isSuccess ? 'Success!' : 'Failed!'),
+        TextButton(
+          onPressed: () {
+            setState(() {
+              isRegistering = !isRegistering;
+            });
+          },
+          child: Text(
+            isRegistering
+                ? 'Already have an account? Sign In'
+                : 'Don\'t have an account? Register',
+          ),
+        ),
+      ],
     );
   }
 }
